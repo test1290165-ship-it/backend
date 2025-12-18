5ca852a8b0259f724f51c3d9c345711b8cf083f1
javascriptmspl <javascript.mspl@gmail.com>
2025-12-18 14:02:57 +0530

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,7 @@
 import User from '../models/user.js';
 import bcrypt from 'bcryptjs'; 
+import jwt from 'jsonwebtoken';
+
 
 // CREATE USER
 export const createUser = async (req, res) => {
@@ -52,3 +54,21 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+export const loginuser=async(req,res)=>{
+  try{
+    const {email,password}=req.body;
+    const user=await User.findOne({email});
+    if(!user) return res.status(400).json({message:"User not found"});
+    const isPasswordValid=await bcrypt.compare(password,user.password);
+    if(!isPasswordValid) return res.status(400).json({message:"Invalid password"});
+    const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{
+      expiresIn:"1h"
+    })
+    res.json({message:"Login successful",token})
+    
+  }catch(error){
+    res.status(500).json({message:error.message})
+  }
+}
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUser, getUsers } from '../controllers/user.js'
+import { createUser, getUsers, loginuser } from '../controllers/user.js'
 const router = express.Router();
 
 /**
@@ -47,5 +47,56 @@ router.post('/create', createUser);
  *         description: List of users
  */
 router.get('/get', getUsers);
+
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     summary: Login User
+ *     tags:
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: user@example.com
+ *               password:
+ *                 type: string
+ *                 example: password123
+ *     responses:
+ *       200:
+ *         description: Login successful, returns user info and token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                       example: 64a1234b567c89d01234ef56
+ *                     name:
+ *                       type: string
+ *                       example: John Doe
+ *                     email:
+ *                       type: string
+ *                       example: user@example.com
+ *       401:
+ *         description: Invalid email or password
+ */
+router.post('/login', loginuser)
 
 export default router;
